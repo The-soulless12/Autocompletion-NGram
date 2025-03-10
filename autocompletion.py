b7e8066956c2c173c5d915c2448d4051576d18c5
The-soulless12 <aklirayane2@gmail.com>
2025-03-10 10:24:51 +0100

    Score = 0.3699 avec bi et uni Gram

--- autocompletion.py
+++ autocompletion.py
@@ -8,14 +8,48 @@ class LaplaceBiGram:
         self.bi_grams = {}
 
     def entrainer(self, data):
-        pass
+        self.uni_grams = {'<s>': 0}
+        self.bi_grams = {}
+
+        for phrase in data:
+            if not phrase:
+                continue
+
+            self.uni_grams['<s>'] += 1
+            prev = '<s>'
+
+            for mot in phrase:
+                # Compter les unigrammes
+                if mot not in self.uni_grams:
+                    self.uni_grams[mot] = 0 
+                self.uni_grams[mot] += 1
+
+                # Compter les bigrammes
+                bigram_key = f"{prev}|{mot}"
+                if bigram_key not in self.bi_grams:
+                    self.bi_grams[bigram_key] = 0
+                self.bi_grams[bigram_key] += 1
+
+                prev = mot
 
     def noter(self, past, current):
-        return 0.0
+        bigram_key = f"{past}|{current}"
+        bigram_count = self.bi_grams.get(bigram_key, 0)
+        unigram_count = self.uni_grams.get(past, 0)
+        vocab_size = len(self.uni_grams)
+
+        # Lissage de Laplace
+        return math.log((bigram_count + 1) / (unigram_count + vocab_size ))
 
     def estimer(self, mots):
+        dernier_mot = mots[-1] if mots else '<s>'
         mots_scores = []
-        # ajouter des tuples (mot, score) à cette liste
+
+        for mot in self.uni_grams.keys():
+            if mot != '<s>':
+                score = self.noter(dernier_mot, mot)
+                mots_scores.append((mot, score))
+
         return sorted(mots_scores, key=lambda tab: tab[1], reverse=True)
 
     def exporter_json(self):
@@ -31,7 +65,7 @@ class Autocompletion():
         self.eval = []
 
     def entrainer(self, url):
-        f = open(url, 'r')
+        f = open(url, 'r', encoding='utf-8')
         data = []
         for l in f: # la lecture ligne par ligne
             phrase = l.strip().lower().split()
@@ -45,7 +79,7 @@ class Autocompletion():
         return mots_scores[:nbr]
 
     def charger_modele(self, url):
-        f = open(url, 'r')
+        f = open(url, 'r', encoding='utf-8')
         data = json.load(f)
         self.modele.importer_json(data)
         f.close()
@@ -56,7 +90,7 @@ class Autocompletion():
         f.close()
 
     def charger_evaluation(self, url):
-        f = open(url, 'r')
+        f = open(url, 'r', encoding='utf-8')
         for l in f: # La lecture se fait ligne par ligne
             info = l.strip().lower().split("	")
             if len(info) < 2 :
@@ -72,7 +106,7 @@ class Autocompletion():
         score = 0.0
         for i in range(n):
             test = S[i]
-            print('p(', test[1], '|',test[0], ')')
+            print('-> Proba(', test[1], '|',test[0], ')')
             res = self.estimer(test[0], m)
             print('found:', res)
             words = [e[0] for e in res]
@@ -90,4 +124,4 @@ if __name__ == '__main__':
     program.entrainer('data/data_train.txt')
     program.sauvegarder_modele('./autocompletion.json')
     program.charger_evaluation('data/data_test.txt')
-    mrr = program.evaluer(80, 10)
+    mrr = program.evaluer(-1, 10) # -1 pour prendre toutes les phrases
